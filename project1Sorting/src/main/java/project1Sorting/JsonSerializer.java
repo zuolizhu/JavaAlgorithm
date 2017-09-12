0353fc60f3fa47fe305fc28fa2a41ff74379bf25
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-09-11 20:28:25 -0400

    delete unrelated files and update version

--- project1Sorting/src/main/java/project1Sorting/JsonSerializer.java
+++ project1Sorting/src/main/java/project1Sorting/JsonSerializer.java
@@ -30,45 +30,9 @@ public class JsonSerializer {
 
         System.out.println("************************************");
 
-        Pet pet = new Pet();
-        pet.setName("Fido");
-        pet.setSpecies("Dog");
-        Person owner = new Person();
-        owner.setLastName("Smith");
-        owner.setFirstName("Mary");
-        pet.setOwner(owner);
-        System.out.println(serializer.serialize(pet));
-
-        System.out.println("************************************");
-                
-        Person marysFriend = new Person();
-        marysFriend.setLastName("Crawford");
-        marysFriend.setFirstName("Joan");
-        List<Person> friends = new Vector<Person>();
-        friends.add(marysFriend); 
-        owner.setFriends(friends);
-        System.out.println(serializer.serialize(pet));
-
-        System.out.println("************************************");
-
-        Student goodStudent = new Student();
-        goodStudent.setLastName("Perez");
-        goodStudent.setFirstName("Susie");
-        int grades[] = { 100, 95, 92, 89, 99 }; 
-        goodStudent.setGrades(grades); 
-        System.out.println(serializer.serialize(goodStudent)); 
-        
-        System.out.println("************************************");
-        System.out.println("------------------------------------");
-        OutList outList = new OutList();
         int inNumber[] = {5, 35, 1, 272, 12, 0, -2, 12};
-        outList.setOutList(inNumber);
-        System.out.println(serializer.serialize(outList));
 
-        System.out.println("------------------------------------");
-        System.out.println("************************************");
         OutJSONObject outJSONObject = new OutJSONObject();
-
         outJSONObject.setOutList(inNumber);
         outJSONObject.setAlgorithm("quick sort");
         outJSONObject.setTimeMS(25);
--- project1Sorting/src/main/java/project1Sorting/Student.java
+++
@@ -1,9 +0,0 @@
-
-package project1Sorting;
-
-public class Student extends Person {
-    private int[] grades; 
-    public Student() { }
-    public int[] getGrades() { return grades; }
-    public void setGrades(int[] grades) { this.grades = grades; } 
-} 
