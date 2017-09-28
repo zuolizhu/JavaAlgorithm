508fb5c3ccc1e78621739e7c6582890853631061
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-09-27 20:42:22 -0400

    project 2 setup with project1 boilerplate, might need somechange

---
+++ project2PriorityQueue/src/main/java/project1Sorting/JsonSerializer.java
@@ -0,0 +1,45 @@
+
+package project1Sorting;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Vector;
+import java.util.List; 
+
+public class JsonSerializer {
+
+    public JsonSerializer() {
+    }
+
+    public String serialize(Object obj) {
+        String str = null; 
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(Include.NON_NULL);
+            str = mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            str = "<error>"; 
+        }
+        return str;
+    }
+
+
+    // test app 
+    public static void main(String[] args) {
+        JsonSerializer serializer = new JsonSerializer();
+
+        System.out.println("************************************");
+
+        int inNumber[] = {5, 35, 1, 272, 12, 0, -2, 12};
+
+        OutJSONObject outJSONObject = new OutJSONObject();
+        outJSONObject.setOutList(inNumber);
+        outJSONObject.setAlgorithm("quick sort");
+        outJSONObject.setTimeMS(25);
+        System.out.println(serializer.serialize(outJSONObject));
+
+        System.out.println("************************************");
+    }
+    
+}
+
---
+++ project2PriorityQueue/src/main/java/project1Sorting/OutJSONObject.java
@@ -0,0 +1,37 @@
+package project1Sorting;
+
+import java.io.Serializable;
+
+public class OutJSONObject implements Serializable{
+    private int[] outList;
+    private String algorithm;
+    private long timeMS;
+
+    public long getTimeMS() {
+        return timeMS;
+    }
+
+    public void setTimeMS(long timeMS) {
+        this.timeMS = timeMS;
+    }
+
+    public OutJSONObject() {
+    }
+
+    public int[] getOutList() {
+        return outList;
+    }
+
+    public void setOutList(int[] outList) {
+        this.outList = outList;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+}
