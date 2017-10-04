0fcf2948d0effe27606557f0d57e79bb6dedbb22
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-10-04 18:54:44 -0400

    trying to figure out the outlist

--- project2PriorityQueue/src/main/java/project2PriorityQueue/JsonDiscerner.java
+++ project2PriorityQueue/src/main/java/project2PriorityQueue/JsonDiscerner.java
@@ -2,6 +2,7 @@ package project2PriorityQueue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class JsonDiscerner {
@@ -12,6 +13,7 @@ public class JsonDiscerner {
     public String discern(String jsonStr) {
         ObjectMapper mapper = new ObjectMapper();
         PriorityQueue<Job> jobQueue = new PriorityQueue<>(new JobComparator());
+        LinkedList<Job> outList = new LinkedList<>();
         // mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         /**
@@ -22,10 +24,10 @@ public class JsonDiscerner {
             for(int i = 0; i < jobList.getInList().size(); i++) {
                 if(jobList.getInList().get(i).getCmd().equals("enqueue")) {
                     jobQueue.add(new Job(jobList.getInList().get(i).getName(), jobList.getInList().get(i).getPri()));
-                    System.out.println("Enqueue hit!");
+//                    System.out.println("Enqueue hit!");
                 } else if (jobList.getInList().get(i).getCmd().equals("dequeue")) {
                     jobQueue.remove();
-                    System.out.println("Dequeue hit!");
+//                    System.out.println("Dequeue hit!");
                 } else {
                     return "List has bad cmd";
                 }
@@ -35,6 +37,7 @@ public class JsonDiscerner {
              * This part is to reach the queue
              */
             while(!jobQueue.isEmpty()){
+                outList.add(jobQueue.peek());
                 System.out.println(jobQueue.peek().getName() + " =->" + jobQueue.peek().getPri());
                 jobQueue.remove();
             }
@@ -46,6 +49,16 @@ public class JsonDiscerner {
 
     }
 
+    private String serialize(Object obj) {
+        String str = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            str = mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            return "{ \"message\" : \"Error serializing result\" } ";
+        }
+        return str;
+    }
 
     // test case
     public static void main(String[] args) {
@@ -55,8 +68,8 @@ public class JsonDiscerner {
         System.out.println("************************************");
 //        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4}, {\"cmd\":\"dequeue\"}]}";
 
-//        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4}]}";
-        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4},{\"cmd\":\"enqueue\", \"name\":\"job2\", \"pri\":3},{\"cmd\":\"dequeue\"},{\"cmd\":\"enqueue\", \"name\":\"job3\", \"pri\":0},{\"cmd\":\"enqueue\", \"name\":\"job4\", \"pri\":1},{\"cmd\":\"dequeue\"}]}";
+        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4}]}";
+//        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4},{\"cmd\":\"enqueue\", \"name\":\"job2\", \"pri\":3},{\"cmd\":\"dequeue\"},{\"cmd\":\"enqueue\", \"name\":\"job3\", \"pri\":0},{\"cmd\":\"enqueue\", \"name\":\"job4\", \"pri\":1},{\"cmd\":\"dequeue\"}]}";
         System.out.println(msg);
         System.out.println(discerner.discern(msg));
         System.out.println("************************************");
