3249ee39ece80abb0c2160cd28393de0e2f1c839
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-10-04 20:52:37 -0400

    RESTFul server setup

--- project2PriorityQueue/src/main/java/project2PriorityQueue/JsonDiscerner.java
+++ project2PriorityQueue/src/main/java/project2PriorityQueue/JsonDiscerner.java
@@ -18,41 +18,40 @@ public class JsonDiscerner {
         // mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         /**
-         * This part is to de serialize the JSON, and store the job object into the queue
+         * This part is to de serialize the JSON, and store the job object into the queue using
+         * java build in priority queue.
          */
         try {
             Job jobList = mapper.readValue(jsonStr, Job.class);
             for(int i = 0; i < jobList.getInList().size(); i++) {
                 if(jobList.getInList().get(i).getCmd().equals("enqueue")) {
                     jobQueue.add(new Job(jobList.getInList().get(i).getName(), jobList.getInList().get(i).getPri()));
-//                    System.out.println("Enqueue hit!");
                 } else if (jobList.getInList().get(i).getCmd().equals("dequeue")) {
                     jobQueue.remove();
-//                    System.out.println("Dequeue hit!");
                 } else {
                     return "List has bad cmd";
                 }
             }
             /**
-             * This part is to reach the queue
+             * This part is to read the object from the queue and store into a temp list.
+             * Then convert list to a JSON
              */
             while(!jobQueue.isEmpty()){
                 tempList.add(jobQueue.peek().getName());
-                System.out.println(jobQueue.peek().getName() + " =->" + jobQueue.peek().getPri());
                 jobQueue.remove();
             }
-
             OutList outList = new OutList();
             outList.setOutList(tempList);
             return serialize(outList);
         }
-
         catch (Exception e) {
             return "{ \"message\" : \"Error - Malformed JSON\" } ";
         }
-
     }
 
+    /**
+     * Generate JSON from java
+     */
     private String serialize(Object obj) {
         String str = null;
         try {
@@ -65,14 +64,15 @@ public class JsonDiscerner {
         return str;
     }
 
-    // test case
+    /**
+     * Local test cases
+     */
     public static void main(String[] args) {
         String msg;
         JsonDiscerner discerner = new JsonDiscerner();
 
         System.out.println("************************************");
 //        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4}, {\"cmd\":\"dequeue\"}]}";
-
 //        msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4}]}";
         msg = "{ \"inList\":[{\"cmd\":\"enqueue\", \"name\":\"job1\", \"pri\":4},{\"cmd\":\"enqueue\", \"name\":\"job2\", \"pri\":3},{\"cmd\":\"dequeue\"},{\"cmd\":\"enqueue\", \"name\":\"job3\", \"pri\":0},{\"cmd\":\"enqueue\", \"name\":\"job4\", \"pri\":1},{\"cmd\":\"dequeue\"}]}";
         System.out.println(msg);
