318a6cb75cf832cdd1dfde00525572a957c43f8c
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-10-24 04:26:08 -0400

    comment added

--- project3ListRelation/src/main/java/project3ListRelation/TopoSorter.java
+++ project3ListRelation/src/main/java/project3ListRelation/TopoSorter.java
@@ -14,7 +14,7 @@ public class TopoSorter {
         SmarterList sl = new SmarterList();
         OutList outList = new OutList();
         /**
-         * Discerner
+         * Discerner the income JSON to object
          */
         try {
             inList = mapper.readValue(jsonStr, InList.class);
@@ -24,24 +24,24 @@ public class TopoSorter {
         }
         /**
          * Topological sort
+         * Add objects from JSON into list with topological sorting
          */
         try {
             for(int i = 0; i < inList.getInList().size(); i++) {
                 sl.add(inList.getInList().get(i).getSmarter().get(0).toString(), inList.getInList().get(i).getSmarter().get(1).toString());
             }
-//            System.out.println(sl.toString());
         }
         catch (Exception e) {
-            return "{ \"message\" : \"Error - Malformed JSON\" } ";
+            return "{ \"message\" : \"Bad list sorting!\" } ";
         }
 
-//        return serialize(inList).replace("inList", "outList");
-//        return "Temp string returned";
         outList.setOutList(sl);
         return serialize(outList);
     }
 
-
+    /**
+     * Converting sorted list to JSON for out streaming
+     */
     private String serialize(Object obj) {
         String str = null;
         try {
@@ -53,26 +53,12 @@ public class TopoSorter {
         return str;
     }
 
-
+    /**
+     * Local test cases
+     */
     public static void main(String[] args) throws Exception {
-//        String case1 = "{ \"inList\" : [ 6, 4, 2, 1 ] }";
-//        String case2 = "{ }";
-//        String case3 = "{ \"inList\" : [ ] }";
-//        String case4 = "{ \"inList\" : }";
-//        String case5 = "{ \"inList\" : [ 2, 4, 6, 8, 10, 12 ] }";
-//        String case6 = "{ \"inList\" : [ 3, 3, 2, 2, 1, 1 ] }";
-//
-//        System.out.println("1:  " + case1 + "  -->  " + new TopoSorter().sort(case1));
-//        System.out.println("2:  " + case2 + "  -->  " + new TopoSorter().sort(case2));
-//        System.out.println("3:  " + case3 + "  -->  " + new TopoSorter().sort(case3));
-//        System.out.println("4:  " + case4 + "  -->  " + new TopoSorter().sort(case4));
-//        System.out.println("5:  " + case5 + "  -->  " + new TopoSorter().sort(case5));
-//        System.out.println("6:  " + case6 + "  -->  " + new TopoSorter().sort(case6));
-
-
-
-        String case7 = "{ \"inList\" : [ {\"smarter\" :[\"Einstein\", \"Feynmann\"]},{\"smarter\" :[\"Feynmann\", \"Gell-Mann\"]} ]}";
-        String case8 = "{ \"inList\" : [ " +
+        String case1 = "{ \"inList\" : [ {\"smarter\" :[\"Einstein\", \"Feynmann\"]},{\"smarter\" :[\"Feynmann\", \"Gell-Mann\"]} ]}";
+        String case2 = "{ \"inList\" : [ " +
                 "{\"smarter\" :[\"Einstein\", \"Feynmann\"]}," +
                 "{\"smarter\" :[\"Feynmann\", \"Gell-Mann\"]}, " +
                 "{\"smarter\" :[\"Gell-Mann\", \"Thorne\"]}, " +
@@ -80,9 +66,10 @@ public class TopoSorter {
                 "{\"smarter\" :[\"Lorentz\", \"Planck\"]}, " +
                 "{\"smarter\" :[\"Hilbert\", \"Noether\"]}," +
                 "{\"smarter\" :[\"Poincare\", \"Noether\"]} ]}";
-        System.out.println(new TopoSorter().sort(case7));
+        System.out.println(new TopoSorter().sort(case1));
         System.out.println("-------------------------------------------------------------");
-        System.out.println(new TopoSorter().sort(case8));
+        System.out.println(new TopoSorter().sort(case2));
+        System.out.println("-------------------------------------------------------------");
     }
 
 }
